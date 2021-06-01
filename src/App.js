748ac7923b7371e7415f8e1c6715412a5f22852a
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-06-01 12:58:52 +0530

    Added functionality based on input count.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,52 @@ import data from "./data";
 
 // Importing Components
 
+// Functional Component
 function App() {
-  return <h2>Lorem Ipsum Generator</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  // Handle Submit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    console.log(amount);
+
+    if (count <= 0) {
+      amount = 1;
+    }
+
+    if (count > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Tired of boring Lorem Ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
+// Default Export
 export default App;
